6daef85364170bb161e5c7b89541551285093042
IvanYang-TW <43823225+IvanYang-TW@users.noreply.github.com>
2018-10-31 17:32:23 +0800

    Implement Gradient Descent in Linear Regression

--- LinearRegression.py
+++ LinearRegression.py
@@ -23,6 +23,49 @@ class LinearRegression:
 
         return self
 
+    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=1e4):
+        """根據訓練數據集 X_train , y_train ，使用梯度下降法訓練 Linear Regression模型"""
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train must be equal to the size of y_train"
+
+        def J(theta, X_b, y):
+            try:
+                return np.sum((y - X_b.dot(theta)) ** 2) /len(y)
+            except:
+                return float('inf')
+
+        def dJ (theta, X_b, y):
+            res = np.empty(len(theta))
+            res[0] = np.sum(X_b.dot(theta) - y)
+            for i in range(1, len(theta)):
+                res[i] = (X_b.dot(theta) - y).dot(X_b[:, i])
+            return res * 2 / len(X_b)
+
+        def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
+
+            theta = initial_theta
+            cur_iter = 0
+
+            while cur_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+                if (abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
+                    break
+
+                cur_iter += 1
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(X_b, y_train, initial_theta, eta, n_iters)
+
+        self.intercept_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+
     def predict(self, X_predict):
         """給定代預測數據集 X_predict , 返回表示 X_predict 的結果向量"""
         assert self.interception_ is not None and self.coef_ is not None, \
